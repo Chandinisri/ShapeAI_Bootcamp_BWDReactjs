c591ca5f071b9c1fd38b2e4734b201453764d258
Chandinisri <85297993+Chandinisri@users.noreply.github.com>
2021-06-06 14:05:28 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer()
+ {
+  var curYr=new Date().getFullYear();
+  return (
+    <footer>
+      <p>©Copyright @ {curYr}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+function Note(){
+  return(
+    <div className="note">
+<p>
+      <big><b> SHAPEAI Javascript and Reactjs BOOTCAMP </b></big>
+
+     <p> 
+       Hi I made this project during the 7 Days Free Bootcamp, conducted by <b> SHAPEAI</b>
+     </p>
+
+     <p>
+       The instructor during the session was Mr. Shaurya Sinha (Data Analyst Intern at Jio). I got to
+       learn a lot during these 7 days and it was an amazing experience learning with SHAPEAI.
+     </p>
+
+     <big><b>Here's the link for you to watch the sessions as well:  </b>
+          <a href="https://www.youtube.com/watch?v=_4Pw80E66cY&list=PL7zl8TDRnbulLetcbkthT0p_IzwgRAYbu"> Web Development with Reactjs 7Days Bootcamp
+          </a>
+     </big>
+
+     <p>
+         <big>I got to have hands on experience on: </big>
+             <li>JavaScript</li>
+             <li>HTML</li>
+             <li>React.js</li>
+     </p>
+
+     <p>
+        During these 7 days, and everything was explained from the very basics so that 
+        anyone with zero experience on programming can learn.I enjoyed these 7 days, you can as well. 
+     </p>
+
+     <p>
+     <big>
+       To register for next free 7 days bootcamp, visit: 
+          <a href="https://www.shapeai.tech"> www.shapeai.tech</a>
+        <big>or follow SHAPEAI on: </big>
+          <li>
+            <a href="https://in.linkedin.com/company/shapeai">LinkedIn</a></li>
+          <li>
+            <a href="https://www.instagram.com/shape.ai/?hl=en">Instagram</a></li>
+          <li> 
+            <a href="https://www.youtube.com/channel/UCTUvDLTW9meuDXWcbmISPdA">YouTube</a></li>
+          <li>
+            <a href="https://github.com/shapeai">GitHub</a></li>
+            </big>
+
+     </p>
+
+
+     </p>
+    </div>
+  );
+}
+export default Note;
